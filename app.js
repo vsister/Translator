dec451e141097fd65d037014451f0e96a925b2e9
vsister <alexandra06.01@mail.ru>
2019-12-16 22:22:25 +0300

    change DB

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ mongoose.connect('mongodb://user:perfectpassword1998@ds042898.mlab.com:42898/gri
 translate.engine = 'yandex';
 translate.key = 'trnsl.1.1.20191210T115214Z.4531ec268421712a.ff53d329c913eb07df8eec7c438028871bcbff83';
 
-const translationSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
+    githubId: String,
     original: String,
     translated: String
 })
 
-const Translation = mongoose.model('Translation ', translationSchema)
+const User = mongoose.model('User', userSchema)
 
-translate(process.env.TEXT, { from: process.env.LANG1, to: process.env.LANG2 }).then(async text =>  {
-    await Translation.findOneAndUpdate({_id:process.env.ID},{translated: text}, function(err, doc){
+translate(process.env.TEXT, { from:process.env.LANG1, to: process.env.LANG2 }).then(async text =>  {
+    await User.findOneAndUpdate({ githubId:process.env.ID},{translated: text}, function(err, doc){
         mongoose.disconnect();
     
         if(err) return console.log(err);
